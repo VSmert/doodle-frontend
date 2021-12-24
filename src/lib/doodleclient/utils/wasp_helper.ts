22953442e94f4de2a9aa96fbccb38bdd98bcc27e
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2021-12-24 04:04:23 +0100

    Minor change

--- src/lib/doodleclient/utils/wasp_helper.ts
+++ src/lib/doodleclient/utils/wasp_helper.ts
@@ -31,8 +31,8 @@ export function generatePrivateKeyAndAddress(): [privateKey: string, publicKey: 
     const seedBuffer = waspClientSeed.generate();
     const addressIndex = 0;
     const { secretKey, publicKey } = waspClientSeed.generateKeyPair(seedBuffer, addressIndex);
-    const generatedAddress = waspClientSeed.generateAddress(seedBuffer, addressIndex);
+    const base58GeneratedAddress = waspClientSeed.generateAddress(seedBuffer, addressIndex);
     const base58PrivateKey = Base58.encode(secretKey);
     const base58PuplicKey = Base58.encode(publicKey);
-    return [base58PrivateKey, base58PuplicKey, generatedAddress];
+    return [base58PrivateKey, base58PuplicKey, base58GeneratedAddress];
 }
