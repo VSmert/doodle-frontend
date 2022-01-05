b4155b289cd4fc182e1158ba36d87d70aad19f35
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2022-01-05 02:17:49 +0100

    Moved proof_of_work.ts to pow_web_worker

--- src/lib/doodleclient/utils/pow_web_worker/pow.worker.ts
+++ src/lib/doodleclient/utils/pow_web_worker/pow.worker.ts
@@ -1,5 +1,5 @@
 import { Log, LogTag } from '../logger';
-import ProofOfWork from '../../wasp_client/proof_of_work';
+import ProofOfWork from './proof_of_work';
 import type { PowWorkerRequest, PowWorkerResponse } from './pow_worker_manager';
 
 const ctx: Worker = self as never;
