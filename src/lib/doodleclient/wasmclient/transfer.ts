aa911b00f110ab89ea81f21479fd59ce02e4012b
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2022-01-07 00:17:16 +0100

    Updated wasmclient

--- src/lib/doodleclient/wasmclient/transfer.ts
+++ src/lib/doodleclient/wasmclient/transfer.ts
@@ -7,7 +7,7 @@ import { Buffer } from "./buffer";
 import { Colors } from "./colors";
 
 export class Transfer {
-    private xfer = new Map<Buffer, wasmclient.Uint64>();
+    private xfer = new Map<string, wasmclient.Uint64>();
 
     static iotas(amount: wasmclient.Uint64): Transfer {
         return Transfer.tokens(Colors.IOTA_COLOR, amount);
@@ -23,16 +23,13 @@ export class Transfer {
         if (color == Colors.IOTA_COLOR) {
             color = Colors.IOTA_COLOR_STRING;
         }
-        this.xfer.set(Base58.decode(color), amount);
+        this.xfer.set(color, amount);
     }
 
-    get(color: string) : wasmclient.Uint64 {
-        if (color == Colors.IOTA_COLOR)
-            color = Colors.IOTA_COLOR_STRING;
-        if (color != Colors.IOTA_COLOR_STRING) 
-            throw new Error("Not Implemented for color other than IOTA");
-        const amount = this.xfer.get(Base58.decode(color));
-        return amount ?? 0n;
+    get(color: string): wasmclient.Uint64 {
+        if (color == Colors.IOTA_COLOR) color = Colors.IOTA_COLOR_STRING;
+        const amount = this.xfer.get(color) ?? 0n;
+        return amount;
     }
 
     // Encode returns a byte array that encodes the Transfer as follows:
@@ -42,25 +39,26 @@ export class Transfer {
     // transfer count. Next for each color emit the 32-byte color value,
     // and then the 8-byte amount.
     encode(): wasmclient.Bytes {
-        const keys = new Array<Buffer>();
+        const keys = new Array<string>();
         for (const [key, val] of this.xfer) {
             // filter out zero transfers
             if (val != BigInt(0)) {
                 keys.push(key);
             }
         }
-        keys.sort((lhs, rhs) => lhs.compare(rhs));
+        keys.sort((lhs, rhs) => lhs.localeCompare(rhs));
 
         let buf = Buffer.alloc(4);
         buf.writeUInt32LE(keys.length, 0);
         for (const key of keys) {
             const val = this.xfer.get(key);
             if (!val) {
-                throw new Error("Transfer.encode: missing amount");
+                throw new Error("Transfer.encode: missing amount?");
             }
+            const keyBuf = Base58.decode(key);
             const valBuf = Buffer.alloc(8);
             valBuf.writeBigUInt64LE(val, 0);
-            buf = Buffer.concat([buf, key, valBuf]);
+            buf = Buffer.concat([buf, keyBuf, valBuf]);
         }
         return buf;
     }
