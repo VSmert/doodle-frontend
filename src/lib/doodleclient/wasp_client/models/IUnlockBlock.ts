30a15eefbaef22eee7c0761db027e7986bc249b8
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2022-01-07 03:58:36 +0100

    Removed wasp_client!

--- src/lib/doodleclient/wasp_client/models/IUnlockBlock.ts
+++
@@ -1,7 +0,0 @@
-import type { Buffer } from "../../wasmclient/buffer";
-export interface IUnlockBlock {
-    type: number;
-    referenceIndex: number;
-    publicKey: Buffer;
-    signature: Buffer;
-}
