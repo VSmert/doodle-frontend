d2e2058871a3495bdc8a534f6f93e339401da353
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2022-01-20 04:07:42 +0100

    Added fix for json stringify bigints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { doodleSetup } from "./doodleSetup";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
+
+doodleSetup();
 
 new Vue({
     render: (h) => h(App),
